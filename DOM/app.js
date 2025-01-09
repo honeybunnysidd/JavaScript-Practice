dd04af8069aeb8f8dbaf5d3e1d9482266cb84d14
honeybunnysidd <siddhartharaghuvanshi01@gmail.com>
2025-01-09 20:14:09 +0530

    update file

---
+++ DOM/app.js
@@ -0,0 +1,10 @@
+/*------------------------------------DOM-------------------------------*/
+
+/*--------------------------getElementById---------------------------*/
+let topImage = document.getElementById("mainImg");
+topImage.src = "assets/creation_1.png";
+
+let para = document.getElementById("description");
+para.innerText = "Hello Developers";
+
+/*--------------------------getElementByClass---------------------------*/
