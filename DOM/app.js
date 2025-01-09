253cd1662b8c09257694b9f26cc09a9c39c06319
honeybunnysidd <siddhartharaghuvanshi01@gmail.com>
2025-01-10 04:24:16 +0530

    update file

--- DOM/app.js
+++ DOM/app.js
@@ -1,17 +1,19 @@
 /*------------------------------------DOM-------------------------------*/
 
 /*--------------------------getElementById---------------------------*/
+/*
 let topImage = document.getElementById("mainImg");
 topImage.src = "assets/creation_1.png";
-
+*/
 // let para = document.getElementById("description");
 // para.innerText = "Hello Developers";
 
 /*--------------------------getElementByClass---------------------------*/
+/*
 let oldImage = document.getElementsByClassName("oldImg");
 //it give collection of objects
 oldImage[2].src = "assets/creation_1.png";
-
+*/
 /*--------------------------getElementByTagName---------------------------*/
 /*
 let element = document.getElementsByTagName("p");
@@ -54,3 +56,45 @@ belowPara.innerHTML = `<u> ${belowPara.innerText}<u>`;
 */
 /*--------------------------textContent---------------------------*/
 //It show hidden content and next line(based on VS code)
+
+/*--------------------------Getter and Setter Attribute---------------------------*/
+/*
+let img = document.querySelector("#mainImg");
+
+console.log(img.getAttribute("src")); //get src of image
+img.setAttribute("src", "assets/creation_1.png"); // set src of image
+*/
+
+/*--------------------------Manipulating Style---------------------------*/
+
+let heading = document.querySelector("h1");
+heading.style.color = "skyblue";
+heading.style.backgroundColor = "grey";
+
+let anchor = document.querySelectorAll(".box  a");
+
+for (let i = 0; i < anchor.length; i++) {
+  anchor[i].style.color = "purple";
+}
+
+/*--------------------------Manipulating classList---------------------------*/
+
+let image = document.querySelector("#mainImg");
+console.log(image.classList); //Empty(zero class exist)
+
+//Add a class
+image.classList.add("mainImg");
+image.classList.add("mainImghai");
+console.log(image.classList);
+
+//Remove a class
+image.classList.remove("mainImg");
+console.log(image.classList);
+
+//Contain a class or not
+image.classList.contains("mainImghai"); //True if exist
+console.log(image.classList.contains("mainImghai"));
+
+//Toggle a class(Add if not exist, Remove if exist)
+image.classList.toggle("mainImg");
+console.log(image.classList);
