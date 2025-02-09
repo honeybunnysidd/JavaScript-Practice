b8bd31f05f69cebeb6bf696c3f8a07299ce32408
honeybunnysidd <siddhartharaghuvanshi01@gmail.com>
2025-02-10 00:23:30 +0530

    add file

---
+++ API/api.js
@@ -0,0 +1,16 @@
+/*
+let jsonRes =
+  '{"fact":"Tests done by the Behavioral Department of the Musuem of Natural History conclude that while a dogs memory lasts about 5 minutes, a cats recall can last as long as 16 hours.","length":175}';
+
+let validRes = JSON.parse(jsonRes);
+console.log(validRes.fact);
+
+let obj = {
+  fact: "Hey I am Siddhartha",
+  length: 36,
+};
+console.log(obj);
+
+let changeIntoJson = JSON.stringify(obj);
+console.log(changeIntoJson);
+*/
